feddd45f76d9ccda14fc467da9cf839d68f1245c
Sean Kennedy <sean@splitleaf.net>
2013-06-14 14:48:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Facebook Tracking Module
+=====================
+
+A Magento module to add Facebook Tracking with Conversion to your store.
---
+++ app/code/community/SUMOHeavy/FacebookTracking/Helper/Data.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * SUMOHeavy_FacebookTracking
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to support@sumoheavy.com so we can send you a copy immediately.
+ *
+ * @category   SUMOHeavy
+ * @package    SUMOHeavy_FacebookTracking
+ * @copyright  Copyright 2013 SUMO Heavy Industries, LLC
+ * @license    http://opensource.org/licenses/osl-3.0.php
+ * @author     Sean Kennedy <support@sumoheavy.com>
+ */
+class SUMOHeavy_FacebookTracking_Helper_Data
+    extends Mage_Core_Helper_Abstract
+{
+    public function getLastOrderTotal()
+    {
+        // Get the order id
+        $orderId = Mage::getSingleton('checkout/session')->getLastRealOrderId();
+        if ($orderId) {
+            $order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
+            $amount = $order->getGrandTotal() - $order->getShippingAmount() - $order->getShippingTaxAmount();
+            return $amount;
+        }
+        return false;
+    }
+}
